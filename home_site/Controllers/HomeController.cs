8f6bccb3d9b558138e4e50061c2481995ca568b8
norberto5 <admin@norberto5.pl>
2019-02-25 18:06:37 +0100

    init commit

---
+++ home_site/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using norberto5.Models;
+
+namespace norberto5.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index() => View();
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Uczę się ASP.NET Core MVC, nie bijcie ;c";
+
+			return View();
+		}
+
+		public IActionResult Contact() => View();
+
+		public IActionResult Privacy() => View();
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
