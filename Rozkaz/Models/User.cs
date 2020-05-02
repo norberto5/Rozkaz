84a834f503fc7a82e3cc9998356c2d763a40422d
norberto5 <admin@norberto5.pl>
2020-05-02 23:52:08 +0200

    Fixed User equatable

--- Rozkaz/Models/User.cs
+++ Rozkaz/Models/User.cs
@@ -3,22 +3,22 @@ using System;
 
 namespace Rozkaz.Models
 {
-    public class User : IComparable, IEquatable<User>
-    {
-        public string Id { get; set; }
+      public class User
+      {
+            public string Id { get; set; }
 
-        [JsonProperty("givenName")]
-        public string Name { get; set; }
+            [JsonProperty("givenName")]
+            public string Name { get; set; }
 
-        public string DisplayName { get; set; }
+            public string DisplayName { get; set; }
 
-        public string Mail { get; set; }
+            public string Mail { get; set; }
 
-        public UnitModel Unit { get; set; }
+            public UnitModel Unit { get; set; }
 
-
-        public int CompareTo(object obj) => Id.CompareTo(obj);
-
-        public bool Equals(User other) => Id == other.Id;
-    }
+            public override bool Equals(object obj) => obj is User user ? Id == user.Id : false;
+            public override int GetHashCode() => HashCode.Combine(Id);
+            public static bool operator ==(User user1, User user2) => user1 is null ? user2 is null : user1.Equals(user2);
+            public static bool operator !=(User user1, User user2) => !(user1 == user2);
+      }
 }
